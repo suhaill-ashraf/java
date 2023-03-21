575e2ec8db3fec4f71db5841d31f99814201a434
Suhail Ashraf <87221973+suhaill-ashraf@users.noreply.github.com>
2023-03-21 10:51:17 +0530

    Add files via upload

---
+++ java_bean simple demo/bean.java
@@ -0,0 +1,29 @@
+public class bean implements java.io.Serializable {
+    private int id;
+    private int roll;
+    private String name;
+    public void setId(int id)
+    {
+        this.id=id;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
